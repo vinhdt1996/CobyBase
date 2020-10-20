17c40e638a0cb3a928a8af30c29bd20d1e83370e
vinn <vinn@vinova.com.sg>
2020-10-20 14:41:21 +0700

    add demo notifications

--- app/src/main/java/com/coby/cobybase/ui/notifications/FragmentNotifications.kt
+++ app/src/main/java/com/coby/cobybase/ui/notifications/FragmentNotifications.kt
@@ -1,15 +1,23 @@
 package com.coby.cobybase.ui.notifications
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageButton
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import com.coby.cobybase.R
 import com.coby.cobybase.base.BaseFragment
 import com.coby.cobybase.databinding.FragmentNotificationsBinding
 import com.coby.cobybase.ext.gone
+import com.coby.cobybase.utils.sendNotification
 
-class FragmentNotifications: BaseFragment<FragmentNotificationsBinding>() {
+class FragmentNotifications : BaseFragment<FragmentNotificationsBinding>() {
 
     override val layoutId: Int
         get() = R.layout.fragment_notifications
@@ -20,5 +28,45 @@ class FragmentNotifications: BaseFragment<FragmentNotificationsBinding>() {
     override fun toolbarFunc(curActivity: AppCompatActivity?, toolbar: Toolbar?) {
         toolbar?.findViewById<AppCompatTextView>(R.id.tvTitle)?.text = "Notifications"
         toolbar?.findViewById<AppCompatImageButton>(R.id.btnBack)?.gone()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        createChannel(
+            getString(R.string.egg_notification_channel_id),
+            "Channel Name"
+        )
+    }
+
+    private fun createChannel(channelId: String, channelName: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_LOW
+            ).apply {
+                enableLights(true)
+                lightColor = Color.RED
+                enableVibration(true)
+                description = "Time for breakfast"
+            }
+            val notificationManager = requireActivity().getSystemService(
+                NotificationManager::class.java
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+    override fun initEventListeners() {
+        binding.btnShowNoti.setOnClickListener {
+            val notificationManager = ContextCompat.getSystemService(
+                requireContext(),
+                NotificationManager::class.java
+            )
+            notificationManager?.sendNotification(
+                "My Body",
+                requireContext()
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/coby/cobybase/utils/NotificationUtils.kt
@@ -0,0 +1,42 @@
+package com.coby.cobybase.utils
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import androidx.core.app.NotificationCompat
+import com.coby.cobybase.R
+import com.coby.cobybase.ui.main.MainActivity
+
+fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context){
+
+    val contentIntent = Intent(applicationContext, MainActivity::class.java)
+    val contentPendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        0,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
+    val eggImage = BitmapFactory.decodeResource(
+        applicationContext.resources,
+        R.drawable.cooked_egg
+    )
+    val bigPicStyle = NotificationCompat.BigPictureStyle()
+        .bigPicture(eggImage)
+        .bigLargeIcon(null)
+
+    val builder = NotificationCompat.Builder(
+        applicationContext,
+        applicationContext.getString(R.string.egg_notification_channel_id)
+    )
+        .setSmallIcon(R.drawable.cooked_egg)
+        .setContentTitle("My Title")
+        .setContentText(messageBody)
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
+        .setStyle(bigPicStyle)
+        .setLargeIcon(eggImage)
+    notify(0, builder.build())
+}
