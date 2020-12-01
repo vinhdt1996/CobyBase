894cb2e9e49c3d866bfa8a672b425d7751edf179
vinn <vinn@vinova.com.sg>
2020-12-01 09:16:11 +0700

    demo Webview

--- app/src/main/java/com/coby/cobybase/ui/authentication/AuthActivity.kt
+++ app/src/main/java/com/coby/cobybase/ui/authentication/AuthActivity.kt
@@ -1,25 +1,87 @@
 package com.coby.cobybase.ui.authentication
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent.ACTION_PICK
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
-import com.coby.cobybase.MainApplication
+import android.provider.MediaStore
+import android.webkit.JavascriptInterface
+import android.widget.Toast
 import com.coby.cobybase.R
 import com.coby.cobybase.base.BaseActivity
 import com.coby.cobybase.databinding.ActivityAuthBinding
-import com.coby.cobybase.ui.main.MainActivity
+
+
+const val SELECT_PHOTO = 1
 
 class AuthActivity : BaseActivity<ActivityAuthBinding>() {
     override val layoutId: Int
         get() = R.layout.activity_auth
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (MainApplication.instance.getUser() != null) {
-            startActivity(Intent(this, MainActivity::class.java).apply {
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-            })
-            finish()
+        binding.webView.settings.apply {
+            javaScriptEnabled = true
+            loadWithOverviewMode = true
+            builtInZoomControls = true
+            allowFileAccess = true
+        }
+        binding.webView.addJavascriptInterface(JavascriptBridge(this), "Android")
+        binding.webView.loadUrl("file:///android_asset/webdemo.html");
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            SELECT_PHOTO -> {
+                if (resultCode == RESULT_OK) {
+                    val selectedImage = intent.data
+                    binding.webView.loadUrl(
+                        "javascript:setFileUri('" + selectedImage.toString().toString() + "')"
+                    )
+                    val path = getRealPathFromURI(this, selectedImage)
+                    binding.webView.loadUrl("javascript:setFilePath('$path')")
+                }
+            }
         }
     }
+
+    fun getRealPathFromURI(context: Context, contentUri: Uri?): String? {
+        var cursor: Cursor? = null
+        return try {
+            val proj = arrayOf(MediaStore.Images.Media.DATA)
+            cursor =
+                context.contentResolver.query(contentUri ?: return null, proj, null, null, null)
+            val columnIndex =
+                cursor?.getColumnIndexOrThrow(MediaStore.Images.Media.DATA) ?: return null
+            cursor.moveToFirst()
+            cursor.getString(columnIndex)
+        } finally {
+            cursor?.close()
+        }
+    }
+
+    inner class JavascriptBridge(private val activity: Activity) {
+        @JavascriptInterface
+        fun showToast(s: String) {
+            Toast.makeText(this@AuthActivity, s, Toast.LENGTH_SHORT).show()
+        }
+
+        @JavascriptInterface
+        fun choosePhoto(): String {
+            val file = "test"
+            val photoPickerIntent = Intent(ACTION_PICK).apply {
+                type = "image/*"
+            }
+            startActivityForResult(photoPickerIntent, SELECT_PHOTO)
+            return file
+        }
+    }
+
 }
+
